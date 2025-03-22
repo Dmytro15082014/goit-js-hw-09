e974fe5efdbd95fa8bf0d5d50c13c82424735c64
Dmytro Teren <leo20091986@gmail.com>
2025-03-22 23:00:06 +0200

    add formData to localStorage

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,20 +1,40 @@
 const formData = {
     email: "",
     message: ""
-}
-let LOCAL_KEY = localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+const LC_KEY = "feedback-form-state";
 
 const formFeed = document.querySelector(".feedback-form");
+const inputMail = formFeed.elements.email;
+const message = formFeed.elements.message;
+
 formFeed.addEventListener("input", handleInput);
 formFeed.addEventListener("submit", handleFeedBack);
 
+saveFeed()
 
 function handleInput(e) {
-    console.log(e);
+    if (e.target.name === "email") {
+        formData.email = e.target.value;
+        localStorage.setItem(LC_KEY, JSON.stringify(formData.email))
+    } else {
+        formData.message = e.target.value;
+        localStorage.setItem(LC_KEY, JSON.stringify(formData.message))
+    }
+    localStorage.setItem(LC_KEY, JSON.stringify(formData))
 }
 
 function handleFeedBack(e) {
     e.preventDefault();
-    console.log(e.target);
+    localStorage.removeItem(LC_KEY)
     formFeed.reset()
+}
+
+function saveFeed() {
+    const saveText = JSON.parse(localStorage.getItem(LC_KEY));
+    console.log(saveText);
+    if (saveText) {
+        inputMail.value = saveText.email;
+        message.value = saveText.message;
+    }
 }
